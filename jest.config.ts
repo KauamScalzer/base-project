4c48bf48c11cdf623058feabc4346523b957fe7c
kauamscalzer <scalzerkauam@gmail.com>
2024-06-23 20:28:33 -0300

    chore: add ts-jest and faker-js

--- jest.config.ts
+++ jest.config.ts
@@ -1,11 +1,12 @@
-import type {Config} from 'jest';
+import type { Config } from 'jest';
 
 const config: Config = {
   clearMocks: true,
   collectCoverage: true,
-  coverageDirectory: "coverage",
-  coverageProvider: "v8",
-  collectCoverageFrom: ['<rootDir>/src/**/*.ts']
+  coverageDirectory: 'coverage',
+  coverageProvider: 'v8',
+  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
+  transform: { '.+\\.ts$': 'ts-jest' }
 };
 
 export default config;

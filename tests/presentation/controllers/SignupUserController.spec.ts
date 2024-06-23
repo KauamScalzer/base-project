e6f27c8589e831f6216c09c81674dc14e491f082
kauamscalzer <scalzerkauam@gmail.com>
2024-06-23 20:29:37 -0300

    test: ensure SignupUserController return 400 if no name is provided

---
+++ tests/presentation/controllers/SignupUserController.spec.ts
@@ -0,0 +1,25 @@
+import { SignUpUserController } from '../../../src/presentation/controllers'
+import { faker } from '@faker-js/faker'
+import { badRequest } from '../../../src/presentation/helpers/httpHelpers'
+import { MissingParamError } from '../../../src/presentation/errors'
+
+type SutTypes = {
+  sut: SignUpUserController
+}
+
+const makeSut = (): SutTypes => {
+  const sut = new SignUpUserController()
+  return { sut }
+}
+
+describe('SignupUserController', () => {
+  test('Should return 400 if no name is provided', async () => {
+    const { sut } = makeSut()
+    const mockRequest: any = {
+      email: faker.internet.email(),
+      password: faker.internet.password() 
+    }
+    const httpResponse = await sut.handle(mockRequest)
+    expect(httpResponse).toEqual(badRequest(new MissingParamError('name')))
+  })
+})

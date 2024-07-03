bca6085a137be4a9f409aa3db15160253f3359a3
kauamscalzer <scalzerkauam@gmail.com>
2024-07-03 00:42:53 -0300

    test: ensure BcryptAdapter return a valid hash on success

--- tests/infra/cryptography/BcryptAdapter.spec.ts
+++ tests/infra/cryptography/BcryptAdapter.spec.ts
@@ -3,6 +3,12 @@ import { BcryptAdapter } from '../../../src/infra/cryptography'
 import bcrypt from 'bcrypt'
 import { throwError } from '../../domain/mocks'
 
+jest.mock('bcrypt', () => ({
+  async hash (): Promise<string> {
+    return 'hash'
+  }
+}))
+
 const salt = 12
 const makeSut = (): BcryptAdapter => {
   return new BcryptAdapter(salt)
@@ -28,6 +34,13 @@ describe('Bcrypt Adapter', () => {
       jest.spyOn(bcrypt, 'hash').mockImplementationOnce(throwError)
       const promise = sut.hash(param)
       await expect(promise).rejects.toThrow()
+    })
+
+    test('Should return a valid hash on success', async ()=> {
+      const sut = makeSut()
+      const param = stringParam()
+      const result = await sut.hash(param)
+      expect(result).toEqual('hash')
     })
   })
 })

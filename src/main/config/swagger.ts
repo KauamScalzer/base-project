33ecac4b5130aa6625aaecbbcbf52f99045a41df
kauamscalzer <scalzerkauam@gmail.com>
2024-07-05 14:11:37 -0300

    feat: add noCache middleware

--- src/main/config/swagger.ts
+++ src/main/config/swagger.ts
@@ -1,7 +1,8 @@
 import { serve, setup } from 'swagger-ui-express'
 import { Express } from 'express'
 import swaggerConfig from '@/main/docs'
+import { noCache } from '../middlewares'
 
 export default (app: Express): void => {
-  app.use('/api-docs', serve, setup(swaggerConfig))
+  app.use('/api-docs', serve, noCache, setup(swaggerConfig))
 }
--- src/main/middlewares/index.ts
+++ src/main/middlewares/index.ts
@@ -1,3 +1,4 @@
 export { bodyParser } from './bodyParser'
 export { cors } from './cors'
 export { contentType } from './contentType'
+export { noCache } from './noCache'
